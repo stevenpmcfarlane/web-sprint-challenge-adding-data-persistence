ce61ca7dbc40a3cba7993b22fd7b5d4a5afbf9d0
Steven <stevenpmcfarlane@gmail.com>
2021-03-19 10:16:53 -0600

    first commit

--- api/server.js
+++ api/server.js
@@ -1,1 +1,8 @@
 // build your server here and require it from index.js
+const db = require("../data/db-config")
+
+const express = require('express');
+
+const server = express();
+
+server.use(express.json());
